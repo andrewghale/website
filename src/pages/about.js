1c2647ab49124e2dee475e9512475ea6c6f2679f
Andrew Hale <andyhalebmx@hotmail.com>
2019-11-07 15:14:00 +0000

    removed backticks in about text

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,10 +13,10 @@ const About = ({ match }) => {
                   <h1 className="page-title">About</h1>
                   <div className="page-text">
                       <p className="text">
-                            Hello, I`&apos;m a Bristol-based front end developer. I have been teaching myself since September `&apos;17.
+                            Hello, I'm a Bristol-based front end developer. I have been teaching myself since September '17.
                       </p>
                       <p className="text">
-                            I`&apos;m also a professional BMX rider. I have performed for films, schools, events and even on a cruise ship;
+                            I'm also a professional BMX rider. I have performed for films, schools, events and even on a cruise ship;
                             see my Instagram for a taste of what I do!
                       </p>
                   </div>
