10061088ab37f7905acce1cbd1076e951f69dc65
Andrew Hale <andyhalebmx@hotmail.com>
2020-01-18 14:59:14 +0000

    test deploy again

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { HashRouter, Switch, Route } from 'react-router-dom';
 import Index from './pages/index';
 import About from './pages/about';
 import Projects from './pages/projects';
-import NumberMachine from './pages/numbermachine';
-import Counter from './pages/counter';
+// import NumberMachine from './pages/numbermachine';
+// import Counter from './pages/counter';
 import Contact from './pages/contact';
 import './styles/app.scss';
 
@@ -15,8 +15,8 @@ const App = () => {
               <Route exact path="/" component={Index} />
               <Route path="/about" component={About} />
               <Route path="/projects" component={Projects} />
-              <Route path="/numbermachine" component={NumberMachine} />
-              <Route path="/counter" component={Counter} />
+              {/* <Route path="/numbermachine" component={NumberMachine} /> */}
+              {/* <Route path="/counter" component={Counter} /> */}
               <Route path="/contact" component={Contact} />
           </Switch>
       </HashRouter>
