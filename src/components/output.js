e7005821bc67ef6b88c18c128dd23b5a23d4432e
Andrew Hale <andyhalebmx@hotmail.com>
2020-01-18 17:11:33 +0000

    Updated build

--- src/components/output.js
+++ src/components/output.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-const Output = (props) => {
-    return (
-        <div>
-            <div className="output-label">{props.text}</div>
-            <p id={`output-${props.name}`} className="output">
-                Enter a number above!
-            </p>
-        </div>
-    )
-}
+const Output = (props) => (
+	<div>
+		<div className="output-label">{props.text}</div>
+		<p id={`output-${props.name}`} className="output">
+			Enter a number above!
+		</p>
+	</div>
+)
 
-export default Output
+export default Output;
